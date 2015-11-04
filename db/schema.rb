df696c60370a878678b5f643e1ab769d5dbfc2a5
hLeePage <hLeepage@yahoo.com>
2015-11-04 16:13:38 -0600

    Database migration

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151104215841) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweets", force: :cascade do |t|
     t.string   "body"
     t.datetime "created_at", null: false
