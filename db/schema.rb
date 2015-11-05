f061e0e4e84f08c83a337c3067c9178ef639fc89
hLeePage <hLeepage@yahoo.com>
2015-11-04 20:31:51 -0600

    Fixed tweet migration by adding user_id, and fixed controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105012349) do
+ActiveRecord::Schema.define(version: 20151105021650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20151105012349) do
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
