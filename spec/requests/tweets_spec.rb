47bbf5c357f3a8dd627281a68179a3c01eec5192
hLeePage <hLeepage@yahoo.com>
2015-11-06 12:57:10 -0600

    Tweak to requests/tweets_spec

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -1,41 +1,65 @@
 require 'rails_helper'
-require 'request_helper'
+
 
 RSpec.describe "Tweets" do
   let(:tweet) { FactoryGirl.create :tweet }
   let(:tweet2) { FactoryGirl.create :tweet }
-
   describe "#index" do
-    it "shows all tweets" do
+    it "show all tweets" do
       tweet
       tweet2
       get tweets_path
       expect(response).to have_http_status(:success)
-      expect(json.count).to eq 2
+      expect(json["data"].count).to eq 2
     end
   end
+
 
   describe "#show" do
     it "shows a tweet" do
       get tweets_path, tweet
-
       expect(response).to have_http_status(:success)
       expect(json["data"][0]["attributes"]["body"]).to eq tweet["body"]
     end
   end
 
-  # describe "#create" do
-  #   before do
-  #     user = FactoryGirl.create(user:"blah", password:"pass")
-  #     token = self.authenticate!("blah", "pass")
-  #     @payload = { "tweet": {
-  #       body: "text"
-  #       }
-  #     }
-  #     end
-  #   it "creates a tweet" do
-  #     post tweets_path, @payload { 'Authorization' => "Bearer #{token}"}
-  #     expect(response).to have_http_status(:created)
-  #   end
-  # end
+
+  describe "#create" do
+
+    it "creates a tweet" do
+binding.pry
+      token = get_token['access_token']
+      post tweet_path, payload, {'Authorization' => "Bearer #{token}"}
+      expect(response).to have_http_status(:created)
+      expect(json["body"]).to eq payload.body
+    end
+  end
 end
+
+
+# RSpec.describe "Tweets" do
+#   let(:payload) {{
+#     "user": {
+#       "email": "Joe@ensminger",
+#       "password": "password"
+#     }
+#   }}
+#   let(:login) {{
+#     "grant_type": "password",
+#     "username": "user100@example.com",
+#     "password": "password"
+#   }}
+#   let(:tweet) {{
+#     "body": "This is in my test suite"
+#   }}
+#   describe "#index" do
+#     it "lists all tweets on page" do
+#       User.create(payload)
+#       post oauth_token_path, login
+#       expect(response).to have_http_status(:success)
+#       expect(json["access_token"]).to_not be_empty
+#       post tweets_path, tweet
+#       expect(response).to have_http_status(:created)
+#     end
+#   end
+# end
