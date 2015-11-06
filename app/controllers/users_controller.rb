4ca953fc9f0a408f150244cb5524c165c605d65e
hLeePage <hLeepage@yahoo.com>
2015-11-06 09:39:41 -0600

    Tweak Users_controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,42 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
-
     render json: @users
   end
-
-  # GET /users/1
-  # GET /users/1.json
-  def show
-    render json: @user
-  end
-
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  # def edit
-  #   @user = User.find(params[:id])
-  #   if @user.update(user_params)
-  #     head :no_content
-  #   else
-  #     render json: @user.errors, status: :unprocessable_entity
-  #   end
-  # end
 
   def follow
     user = User.find(params[:user_id])
@@ -45,9 +13,41 @@ class UsersController < ApplicationController
 
   def unfollow
     user = User.find(params[:user_id])
-    current_user.stop_follow(user)
+    current_user.stop_following(user)
   end
 
+  def show
+    render json: @user
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      head :no_content
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+    head :no_content
+  end
+
+  #def self.authenticate!(email, password)
+  # user = User.find_by_email(email)
+  #user.authenticate(password)
+  #end
   private
 
   def set_user
