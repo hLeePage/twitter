8a4d8bb4362ebed521bbf48bc9377215267f79e6
hLeePage <hLeepage@yahoo.com>
2015-11-06 12:51:19 -0600

    Add to spec/request_helper.rb

--- spec/request_helper.rb
+++ spec/request_helper.rb
@@ -3,3 +3,14 @@ require 'rails_helper'
 def json
   JSON.parse(response.body)
 end
+
+def get_token
+  user = FactoryGirl.create(password: "password")
+  payload = {
+    grant_type: "password",
+    username: "user100@example.com",
+    password: "password"
+  }
+  post oauth_token_path, payload
+  json
+end
