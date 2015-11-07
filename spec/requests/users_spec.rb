2c80567fb3b272d73f5771bd488dd8c972a625f1
hLeePage <hLeepage@yahoo.com>
2015-11-06 19:22:55 -0600

    Additional tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe "Users" do
       user1
       get users_path
       expect(response).to have_http_status(:success)
-      expect(json["data"].count).to eq 1
+      expect(json.count).to eq 1
     end
   end
 
@@ -45,10 +45,24 @@ RSpec.describe "Users" do
     it "shows a user" do
       get users_path, user1
       expect(response).to have_http_status(:success)
-      expect(json["data"][0]["attributes"]["email"]).to eq user1["email"]
+      expect(json[0]["username"]).to eq user1["username"]
+    end
+  end
+  describe "#follow" do
+    let(:token) { FactoryGirl.create :access_token}
+    let(:user) { FactoryGirl.create :user }
+    it "allows another user to follow" do
+      put  user_follow_path(user.id), {}, {'authorization' => "Bearer #{token.token}"}
+      expect(response).to have_http_status(:success)
     end
   end
 
-
-
+  describe "#unfollow" do
+    let(:token) { FactoryGirl.create :access_token}
+    let(:user) { FactoryGirl.create :user }
+    it "allows another user to follow" do
+      put  user_unfollow_path(user.id), {}, {'authorization' => "Bearer #{token.token}"}
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
