427f6e6a820fbade7b5f303671ef9664c40208e1
hLeePage <hLeepage@yahoo.com>
2015-11-07 11:52:30 -0600

    Changed my schema.rb file to include Users.auth_token. Also added 'add_foreign_key tweets users'.

--- db/schema.rb
+++ db/schema.rb
@@ -70,17 +70,21 @@ ActiveRecord::Schema.define(version: 20151105021650) do
   add_index "oauth_applications", ["uid"], name: "index_oauth_applications_on_uid", unique: true, using: :btree
 
   create_table "tweets", force: :cascade do |t|
-    t.string   "body"
+    t.text     "body"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
   end
+
+  add_index "tweets", ["user_id"], name: "index_tweets_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
+    t.string   "auth_token"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
   end
 
+  add_foreign_key "tweets", "users"
 end
